38908eb10e491ebed69c7f81a3400baf16e73e7e
Lamis <lamis.beirkdar@gmail.com>
2013-10-01 22:03:32 +0200

    scrolling menu

---
+++ html/js/main.js
@@ -0,0 +1,20 @@
+$(function () {
+
+    var $doc = $(document);
+
+
+    $doc.on('scroll', scrollMain);
+
+    var nav = $('.navigation');
+
+    function scrollMain() {
+        if (!nav.hasClass('is-fixed') && $doc.scrollTop() >= 647) attachNav();
+        if (nav.hasClass('is-fixed') && $doc.scrollTop() < 647) attachNav();
+    }
+
+    function attachNav() {
+        nav.toggleClass('is-fixed');
+    }
+});
+
+
