325885a1d9fcd71ddba02a3c83319320cd2d8a22
lamisb <lamis.b@live.com>
2013-10-26 14:19:55 +0200

    push changes

--- php/sites/all/themes/monria/templates/page.tpl.php
+++ php/sites/all/themes/monria/templates/page.tpl.php
@@ -1,4 +1,12 @@
-<div class="gamecontent_reposition_inner">
+<?php
+/**
+ * @file
+ * Returns the HTML for a single Drupal page.
+ *
+ * Complete documentation for this file is available online.
+ * @see https://drupal.org/node/1728148
+ */
+?>
 
     <div class="navigation">
 
