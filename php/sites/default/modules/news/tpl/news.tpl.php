a96a138525ef55008eb90af6bcb34cfe393b8bca
lamisb <lamis.b@live.com>
2013-10-27 01:12:03 +0200

    news inner page

---
+++ php/sites/default/modules/news/tpl/news.tpl.php
@@ -0,0 +1,88 @@
+<div class="news_content">
+
+    <?php foreach ($nodes as $node): ?>
+        <div class="news_block">
+            <div class="img_wrapper">
+                <div class="border"></div>
+                <img height="152" width="225" src="<?php echo base_path().'/sites/default/files/'.$node->news_image['und'][0]['filename']; ?>">
+            </div>
+            <div class="text_wrapper">
+                <h4> <a href="<?php echo base_path() ?>/node/<?php echo $node->nid;?>"> <?php echo $node->title;   ?></a></h4>
+                <?php echo $node->body['und'][0]['safe_summary'];   ?>
+            </div>
+        </div>
+    <?php endforeach; ?> <?php foreach ($nodes as $node): ?>
+        <div class="news_block">
+            <div class="img_wrapper">
+                <div class="border"></div>
+                <img height="152" width="225" src="<?php echo base_path().'/sites/default/files/'.$node->news_image['und'][0]['filename']; ?>">
+            </div>
+            <div class="text_wrapper">
+                <?php echo $node->body['und'][0]['safe_summary'];   ?>
+            </div>
+        </div>
+    <?php endforeach; ?> <?php foreach ($nodes as $node): ?>
+        <div class="news_block">
+            <div class="img_wrapper">
+                <div class="border"></div>
+                <img height="152" width="225" src="<?php echo base_path().'/sites/default/files/'.$node->news_image['und'][0]['filename']; ?>">
+            </div>
+            <div class="text_wrapper">
+                <?php echo $node->body['und'][0]['safe_summary'];   ?>
+            </div>
+        </div>
+    <?php endforeach; ?> <?php foreach ($nodes as $node): ?>
+        <div class="news_block">
+            <div class="img_wrapper">
+                <div class="border"></div>
+                <img height="152" width="225" src="<?php echo base_path().'/sites/default/files/'.$node->news_image['und'][0]['filename']; ?>">
+            </div>
+            <div class="text_wrapper">
+                <?php echo $node->body['und'][0]['safe_summary'];   ?>
+            </div>
+        </div>
+    <?php endforeach; ?> <?php foreach ($nodes as $node): ?>
+        <div class="news_block">
+            <div class="img_wrapper">
+                <div class="border"></div>
+                <img height="152" width="225" src="<?php echo base_path().'/sites/default/files/'.$node->news_image['und'][0]['filename']; ?>">
+            </div>
+            <div class="text_wrapper">
+                <?php echo $node->body['und'][0]['safe_summary'];   ?>
+            </div>
+        </div>
+    <?php endforeach; ?> <?php foreach ($nodes as $node): ?>
+        <div class="news_block">
+            <div class="img_wrapper">
+                <div class="border"></div>
+                <img height="152" width="225" src="<?php echo base_path().'/sites/default/files/'.$node->news_image['und'][0]['filename']; ?>">
+            </div>
+            <div class="text_wrapper">
+                <?php echo $node->body['und'][0]['safe_summary'];   ?>
+            </div>
+        </div>
+    <?php endforeach; ?> <?php foreach ($nodes as $node): ?>
+        <div class="news_block">
+            <div class="img_wrapper">
+                <div class="border"></div>
+                <img height="152" width="225" src="<?php echo base_path().'/sites/default/files/'.$node->news_image['und'][0]['filename']; ?>">
+            </div>
+            <div class="text_wrapper">
+                <?php echo $node->body['und'][0]['safe_summary'];   ?>
+            </div>
+        </div>
+    <?php endforeach; ?> <?php foreach ($nodes as $node): ?>
+        <div class="news_block">
+            <div class="img_wrapper">
+                <div class="border"></div>
+                <img height="152" width="225" src="<?php echo base_path().'/sites/default/files/'.$node->news_image['und'][0]['filename']; ?>">
+            </div>
+            <div class="text_wrapper">
+                <?php echo $node->body['und'][0]['safe_summary'];   ?>
+            </div>
+        </div>
+    <?php endforeach; ?>
+    <div class="read_more_news">
+        <a href="#"></a>
+    </div>
+</div>
