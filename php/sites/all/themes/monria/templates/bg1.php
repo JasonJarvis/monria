66d142bd206088cfb7dc54904c822fad377f14bd
lamisb <lamis.b@live.com>
2013-11-02 15:14:13 +0200

    Seperate menu and bg1 content from templates

---
+++ php/sites/all/themes/monria/templates/bg1.php
@@ -0,0 +1,17 @@
+
+
+    <span  class="card_left_span">Screenshots</span>
+    <span class="card_middle_span">Video</span>
+    <span class="card_right_span">Story</span>
+
+    <a href="<?php echo base_path() ?>media" class="card_left">
+
+    </a>
+    <a href="<?php echo base_path() ?>media/video" class="card_middle">
+
+    </a>
+    <a href="download/?ccode=affimoon130401" class="card_right">
+
+    </a>
+
+
---
+++ php/sites/all/themes/monria/templates/menu.php
@@ -0,0 +1,7 @@
+<a class="home" href="<?php echo base_path() ?>"></a>
+<a class="download" href=""></a>
+<a class="forum" href=""></a>
+<a class="login" href=""></a>
+<a class="guide" href=""></a>
+<a class="media" href="<?php echo base_path() ?>media"></a>
+<a class="about" href=""></a>
