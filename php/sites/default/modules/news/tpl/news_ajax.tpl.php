4b1b29571139a1428b1183153ff46d5f376b42e1
lamisb <lamis.b@live.com>
2013-11-03 21:24:44 +0200

    fix news titles to middle in recent news

--- php/sites/default/modules/news/tpl/news_ajax.tpl.php
+++ php/sites/default/modules/news/tpl/news_ajax.tpl.php
@@ -1,12 +1,13 @@
 <link rel="stylesheet" href="<?php echo base_path().drupal_get_path('module', 'news'); ?>/css/news_ajax.css"></style>
 <?php foreach ($nodes as $node): ?>
     <div class="news_ajax_block">
-
+<div class="block_news_left">
             <img height="65" width="90"
                  src="<?php echo base_path() . '/sites/default/files/' . $node->news_image['und'][0]['filename']; ?>" >
-
+</div>
+<div class="block_news_right">
 
          <a href="<?php echo base_path() ?>news_detail/<?php echo $node->nid; ?>"> <?php echo $node->title; ?></a>
-
+</div>
     </div>
 <?php endforeach; ?>
