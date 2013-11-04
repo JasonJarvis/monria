7c9918218c8c7877f58a746b9a193a3a6d759804
lamisb <lamis.b@live.com>
2013-11-04 11:50:30 +0200

    Read more inner news

--- php/sites/default/modules/news/tpl/news.tpl.php
+++ php/sites/default/modules/news/tpl/news.tpl.php
@@ -1,3 +1,17 @@
+<style>
+    .read_more {
+        bottom: 10px;
+        right:30px;
+        color: #ADD8E6 !important;
+        font-size: 12px;
+        position: absolute;
+        text-decoration: none;
+
+    }
+    .read_more:hover {
+        text-decoration: underline;
+    }
+</style>
 <div class="news_title">
 
 </div>
@@ -14,7 +28,10 @@
                 <h4> <a href="<?php echo base_path() ?>news_detail/<?php echo $node->nid; ?>"> <?php echo $node->title;   ?></a></h4>
                 <?php echo $node->body['und'][0]['safe_summary'];   ?>
             </div>
+
+            <a   class="read_more"  href="<?php echo base_path() ?>news_detail/<?php echo $node->nid; ?>"> Read More</a>
         </div>
+
     <?php endforeach; ?>
 
 </div>
