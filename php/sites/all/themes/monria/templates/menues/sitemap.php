d9277e0507776b7846fd110fb9260d3658c45b46
lamisb <lamis.b@live.com>
2013-11-04 11:36:43 +0200

    sitemap move

---
+++ php/sites/all/themes/monria/templates/menues/sitemap.php
@@ -0,0 +1,37 @@
+<div id="site_map">
+    <div class="footer_list_container">
+        <ul>
+            <li><span> Main </span></li>
+            <li><a href="<?php echo base_path(); ?>">Home</a></li>
+            <li><a href="<?php echo base_path(); ?>news">News</a></li>
+            <li><a href="http://monria.com/forum">Forum</a></li>
+            <li><a href="<?php echo base_path(); ?>download">Download</a></li>
+        </ul>
+    </div>
+    <div class="footer_list_container">
+        <ul>
+            <li><span> Media</span></li>
+            <li><a href="<?php echo base_path(); ?>screenshot">Screenshots</a></li>
+            <li><a href="<?php echo base_path(); ?>video">Videos</a></li>
+            <li><a href="<?php echo base_path(); ?>story">Story</a></li>
+        </ul>
+    </div>
+    <div class="footer_list_container">
+        <ul>
+            <li><span> Reach Us</span></li>
+            <li><a href="<?php echo base_path(); ?>about">About</a></li>
+            <li><a href="<?php echo base_path(); ?>">Contact</a></li>
+            <li><a href="<?php echo base_path(); ?>">Guide</a></li>
+            <li><a href="<?php echo base_path(); ?>">Support</a></li>
+        </ul>
+    </div>
+    <div class="footer_list_container">
+        <ul>
+            <li><span> Social</span></li>
+            <li><a href="https://www.facebook.com/monriathegame">Facebook</a></li>
+            <li><a href="https://twitter.com/monriathegame">Twitter</a></li>
+            <li><a href="https://www.youtube.com/monriathegame">Youtube</a></li>
+        </ul>
+    </div>
+    <div class="blank"></div>
+</div>
