212138090347ee9360f5448d59e46959f606e9b4
lamisb <lamis.b@live.com>
2013-11-17 18:53:59 +0200

    menu links

--- php/sites/all/themes/monria/templates/menues/menu.php
+++ php/sites/all/themes/monria/templates/menues/menu.php
@@ -1,7 +1,7 @@
 <a class="home" href="<?php echo base_path() ?>"></a>
-<a class="download" href="<?php echo base_path() ?>download"></a>
-<a class="forum" href="http://monria.com/forum"></a>
+<a class="download" href="<?php echo base_path() ?>news"></a>
+<a class="forum" href="https://account.entropiauniverse.com/new-account/?ccode=<?php echo $code; ?>"></a>
 <a class="login" href=""></a>
 <a class="guide" href="<?php echo base_path() ?>guide"></a>
-<a class="support" href="https://account.entropiauniverse.com/support-faq/?ccode=<?php echo $code; ?>"></a>
+<a class="support" href="<?php echo base_path() ?>forum"></a>
 <a class="about" href="<?php echo base_path() ?>about"></a>
